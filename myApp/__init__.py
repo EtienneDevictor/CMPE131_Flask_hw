189782bb0dc2fecc611cd860d76d16beeac386f5
Etienne Devictor <etienne.devictor@sjsu.edu>
2021-10-27 19:51:21 -0700

    feat- added code to to the __init__.py and routes.py to get a working cite -style lots of syntax fixes aswell

--- myApp/__init__.py
+++ myApp/__init__.py
@@ -1,1 +1,18 @@
-X
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#creating flask object
+myObj = Flask(__name__)
+
+myObj.config.from_mapping(
+	Secret_Key = 'any string works',
+	#Location the database will be created 
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),
+	SQLALCHEMY_TRACK_MODIFICATIONS = False)
+
+db = SQLAlchemy(myObj)
+
+from myApp import routes #, models
