0b66df130547757aa7ee289f8c9d67280d0e78b6
Etienne Devictor <etienne.devictor@sjsu.edu>
2021-11-05 00:02:46 -0700

    rework- the exist method only checks for reused city names, City primary key changed to id, City rank list query through the database ordered by rank

--- myApp/models.py
+++ myApp/models.py
@@ -1,19 +1,20 @@
 from myApp import db
 
 class City(db.Model):
+	id = db.Column(db.Integer(), primary_key=True)
 	city = db.Column(db.String(64), index=True, unique=True)
-	rank = db.Column(db.Integer, primary_key=True)
+	rank = db.Column(db.Integer, index=True)
 	visited = db.Column(db.Boolean)
 
 	def __repr__(self):
 		return f'{self.city}'
 
-	def exists(self, city_name, rank):
+	def exists(self, city_name):
 		cities = self.query.all()
 		for city in cities:
 			if city_name == city.city:
-				return -1
-			if rank == city.rank:
-				return 1
-		return 0
+				return True
+		return False
 
+	
+ 					
